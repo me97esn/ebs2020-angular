98c63e6a1107185e5b5a57f98a398c6d58e61502
Emil Stenberg <esandin@gmail.com>
2014-05-05 12:38:19 +0200

    now displaying fetched schema :)

--- app/scripts/controllers/announcementcase.js
+++ app/scripts/controllers/announcementcase.js
@@ -1,8 +1,36 @@
 'use strict';
 
 angular.module('ebs2020AngularApp')
-  .controller('AnnouncementcaseCtrl', function ($scope, Restapi) {
-    var caseid = 24461;
-    $scope.announcementCase = new Restapi.DataModel({id:caseid, module: 'AnnouncementCase'});
-    $scope.announcementCase.fetch();
-  });
+	.controller('AnnouncementcaseCtrl', function($scope, Restapi, Settings) {
+		console.log('TODO: Read id from url, here is hard coded');
+		var caseid = 24461;
+		$scope.announcementCase = new Restapi.DataModel({
+			id: caseid,
+			module: 'AnnouncementCase'
+		});
+		$scope.announcementCase.fetch();
+
+		$scope.schemaModel = new Restapi.SchemaModel({
+			id: caseid,
+			module: 'AnnouncementGeneralSection',
+			valueTwo: Settings.applicationName
+		})
+		$scope.schemaModel.fetch({
+			success: function(){
+				$scope.$digest();
+			},
+			error: function(err) {
+				// Could not find schema, try with the default
+				$scope.schemaModel.valueTwo = 'default';
+				$scope.schemaModel.fetch({
+					success: function() {
+						$scope.$digest();
+						// Found schema!
+					},
+					error: function(err) {
+						// Could not find schema, no more to try
+					}
+				});
+			}.bind(this)
+		});
+	});
--- app/scripts/services/restapi.js
+++ app/scripts/services/restapi.js
@@ -1,72 +1,90 @@
 'use strict';
 
 angular.module('ebs2020AngularApp')
-  .service('Restapi', function Restapi(Settings) {
-  	var settings = Settings;
-  	this.BaseModel = Backbone.AssociatedModel.extend({
-            /******
-            * Note, do not override this method unless you're absolutely certain about what you're doing!
-            * A safer way is to implement the onInit method on your models.
-            */
-            initialize: function (attributes) {
-                if (this.beforeInit) {
-                    this.beforeInit(attributes);
-                }
-                Backbone.Model.prototype.initialize.call(this, arguments);
+	.service('Restapi', function Restapi(Settings) {
+		var settings = Settings;
+		this.BaseModel = Backbone.AssociatedModel.extend({
+			/******
+			 * Note, do not override this method unless you're absolutely certain about what you're doing!
+			 * A safer way is to implement the onInit method on your models.
+			 */
+			initialize: function(attributes) {
+				if (this.beforeInit) {
+					this.beforeInit(attributes);
+				}
+				Backbone.Model.prototype.initialize.call(this, arguments);
 
-                if (!this.id) {
-                    this.id = '-1';
-                }
+				if (!this.id) {
+					this.id = '-1';
+				}
 
-                this.originalSave = this.save;
+				this.originalSave = this.save;
 
-                this.save = function (key, val, options) {
-                    // TODO this should be removed?
-                    if (this.id === '-1') {
-                        delete this.id;
-                    }
+				this.save = function(key, val, options) {
+					// TODO this should be removed?
+					if (this.id === '-1') {
+						delete this.id;
+					}
 
-                    this.originalSave(key, val, options);
-                };
+					this.originalSave(key, val, options);
+				};
 
-                this.idAttribute = 'id';
-                this.revAttribute = '_rev';
-                console.log('TODO: add ioBind to model!');
+				this.idAttribute = 'id';
+				this.revAttribute = '_rev';
+				console.log('TODO: add ioBind to model!');
 
-                // this.ioBind('serverdelete', conosleIo.socketio, this.doTheFetch);
-                // this.ioBind('servercreate', conosleIo.socketio, this.doTheFetch);
-                // this.ioBind('serverupdate', conosleIo.socketio, this.doTheFetch);
+				// this.ioBind('serverdelete', conosleIo.socketio, this.doTheFetch);
+				// this.ioBind('servercreate', conosleIo.socketio, this.doTheFetch);
+				// this.ioBind('serverupdate', conosleIo.socketio, this.doTheFetch);
 
-                if (attributes) {
-                    this.id = attributes[this.idAttribute] || attributes.id || attributes._id; // We always want to use 'id' as our it attribute
-                }
+				if (attributes) {
+					this.id = attributes[this.idAttribute] || attributes.id || attributes._id; // We always want to use 'id' as our it attribute
+				}
 
-                if (this.onInit) {
-                    this.onInit(attributes);
-                }
-            },
+				if (this.onInit) {
+					this.onInit(attributes);
+				}
+			},
 
-            doTheFetch: function () {
-                console.log('do the fetch ♫♪♩♫♬');
-                this.fetch({
-                    reset: true,
-                    success: function () {
-                        console.debug('SocketIO - Model - Fetched: ', this);
-                    }.bind(this)
-                });
-            }
-        });
+			doTheFetch: function() {
+				console.log('do the fetch ♫♪♩♫♬');
+				this.fetch({
+					reset: true,
+					success: function() {
+						console.debug('SocketIO - Model - Fetched: ', this);
+					}.bind(this)
+				});
+			}
+		});
+		this.SchemaModel = this.BaseModel.extend({
 
-  	this.DataModel = this.BaseModel.extend({
-        url: function () {
-            if (this.id) {
-                return settings.getRestUrl('EntityREST/' + (this.module || this.get('module')) + '/' + this.id);
-                //return settings.getRestUrl('EntityREST/AnnouncementCase/' + this.announcementId + '/' + this.module + '/' + this.id);
-            }
-            else {
-                return settings.getRestUrl('EntityREST/' + (this.module || this.get('module')));
-            }
-        }
-    });
-    // AngularJS will instantiate a singleton by calling "new" on this function
-  });
+			url: function() {
+				if (this.valueTwo) {
+					return settings.getRestUrl('ComponentSchemaREST?valueOne=' + this.module + '&valueTwo=' + this.valueTwo + '&caseId=' + this.id);
+				} else if (this.id) {
+					return settings.getRestUrl('ComponentSchemaREST?valueOne=' + this.module + '&valueTwo=default');
+				} else {
+					return settings.getRestUrl('ComponentSchemaREST/' + this.module);
+				}
+			},
+
+			initialize: function(options) {
+				this.module = options.module;
+				this.id = options.id;
+				this.valueTwo = options.valueTwo;
+			}
+		});
+
+
+		this.DataModel = this.BaseModel.extend({
+			url: function() {
+				if (this.id) {
+					return settings.getRestUrl('EntityREST/' + (this.module || this.get('module')) + '/' + this.id);
+					//return settings.getRestUrl('EntityREST/AnnouncementCase/' + this.announcementId + '/' + this.module + '/' + this.id);
+				} else {
+					return settings.getRestUrl('EntityREST/' + (this.module || this.get('module')));
+				}
+			}
+		});
+		// AngularJS will instantiate a singleton by calling "new" on this function
+	});
