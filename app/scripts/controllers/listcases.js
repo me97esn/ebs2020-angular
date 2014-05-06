f3b490708bafaddf54cc71b4f7d54e0cfd191f78
Emil Stenberg <esandin@gmail.com>
2014-05-06 13:45:18 +0200

    can now create announcement

--- app/scripts/controllers/listcases.js
+++ app/scripts/controllers/listcases.js
@@ -1,10 +1,14 @@
 'use strict';
 
 angular.module('ebs2020AngularApp')
-  .controller('ListcasesCtrl', function ($scope, Restapi) {
+  .controller('ListcasesCtrl', function ($scope, Restapi, $http, $location) {
     $scope.cases = Restapi.listAnnouncementCases.query();
     $scope.createAnnouncement = function(){
-    	// https://localhost:4000/restapi/FlowEngineREST/new/CREATE_ANNOUNCEMENT/AnnouncementCase/25250
-    	console.log('TODO: create it');
+    	$http.post('/restapi/EntityREST/AnnouncementCase', {className: "se.esf.ebs2020.entity.announcement.AnnouncementCase"}).success(function(response){
+    		$http.put('/restapi/FlowEngineREST/new/CREATE_ANNOUNCEMENT/AnnouncementCase/' + response.id, response).success(function(){
+    			console.log('TODO goto announcement');
+    			$location.path('/AnnouncementCase/' + response.id);
+    		})
+    	});
     }
   });
